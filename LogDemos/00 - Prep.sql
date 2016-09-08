e982c476ce3ebd6dfbda26fd4e82daff796ab57e
Brian Hansen <brian@tf3604.com>
2016-09-08 18:36:58 -0500

    Fixup demo solution.

--- LogDemos/00 - Prep.sql
+++ LogDemos/00 - Prep.sql
@@ -5,3 +5,4 @@ alter database CorpDB set offline with rollback immediate;
 restore database CorpDB from disk = 'C:\data\sql2016\backup\CorpDB.bak'
 with replace;
 
+alter database CorpDB set compatibility_level = 130;
