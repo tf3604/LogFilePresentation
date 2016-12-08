942bac37a9fe8b59ea287e2ee26565554868316c
Brian Hansen <brian@tf3604.com>
2016-12-07 18:45:42 -0600

    Add WITH MOVE to restore script.

--- LogDemos/00 - Prep.sql
+++ LogDemos/00 - Prep.sql
@@ -3,6 +3,8 @@ use master;
 alter database CorpDB set offline with rollback immediate;
 
 restore database CorpDB from disk = 'C:\data\sql2016\backup\CorpDB.bak'
-with replace;
+with move 'CorpDB' to 'c:\data\sql2016\data\CorpDB.mdf',
+move 'CorpDB_log' to 'c:\data\sql2016\log\CorpDB_log.ldf',
+replace;
 
 alter database CorpDB set compatibility_level = 130;
