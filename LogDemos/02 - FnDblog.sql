a80a58a82fc630e0628882bfb1449995d31112fd
Brian Hansen <brian@tf3604.com>
2016-12-13 19:51:58 -0600

    Update LSN values in fn_dblog() demo.

--- LogDemos/02 - FnDblog.sql
+++ LogDemos/02 - FnDblog.sql
@@ -39,10 +39,10 @@ from fn_dblog(null, null);
 -- and in the second statement the values will need to be prefixed with '0x'.
 
 select *
-from fn_dblog('1242:880:314', '1243:919:268');
+from fn_dblog('283:2192:1', '283:2200:2');
 
 select *
-from fn_dblog('0x000004da:00000370:013a', '0x000004db:00000397:010c');
+from fn_dblog('0x0000011b:00000890:0001', '0x0000011b:00000898:0002');
 
 -- By default, fn_dblog only returns records from the active portion of the log.  If trace flag 2537 is enabled,
 -- fn_dblog will return records from the inactive portion as well.
